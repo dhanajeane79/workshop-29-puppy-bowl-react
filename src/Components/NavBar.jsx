39df89d9719e2c88a3161b3878f393ac3555696c
dhanajeane79 <dhanajeane79@gmail.com>
2023-08-05 20:41:23 -0400

    Updated styling

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -3,10 +3,19 @@ import { Link } from "react-router-dom";
 
 export default function NavBar() {
     return (
-        <nav>
-            <Link to="/">Home</Link>
-            <Link to="/players">All Players</Link>
-            <Link to="/newPlayer">Add Player</Link>
-        </nav>
-    )
+    <nav className="navbar">
+      <div className="navbar-logo">Puppy Bowl 2023</div>
+      <ul className="navbar-links">
+        <li>
+          <Link to="/" className="navbar-link">Home</Link>
+        </li>
+        <li>
+          <Link to="/players" className="navbar-link">All Players</Link>
+        </li>
+        <li>
+          <Link to="/newPlayer" className="navbar-link">Add Player</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
