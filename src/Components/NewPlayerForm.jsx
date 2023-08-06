0a85dff207ef6fda3649a640487abc113b2edd42
dhanajeane79 <dhanajeane79@gmail.com>
2023-08-06 11:14:05 -0400

    Added Image preview when breed is entered

--- src/Components/NewPlayerForm.jsx
+++ src/Components/NewPlayerForm.jsx
@@ -1,89 +1,100 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
-import { useState } from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import BreedImage from "./BreedImage";
 
 export default function NewPlayerForm({ APIURL }) {
-    const [name, setName] = useState("");
-    const [breed, setBreed] = useState("");
-    const [status, setStatus] = useState("");
-    const [imageUrl, setImgUrl] = useState("");
-    const [teamId, setTeamId] = useState("");
-    const navigate = useNavigate();
-    
-    async function handleSubmit(event) {
-        event.preventDefault();
-        
-        const newPlayer = { name, breed, status, imageUrl, teamId };
+  const [name, setName] = useState("");
+  const [breed, setBreed] = useState("");
+  const [status, setStatus] = useState("");
+  const [imageUrl, setImgUrl] = useState("");
+  const [teamId, setTeamId] = useState("");
+  const navigate = useNavigate();
 
-        if ((status !=='bench') || (status !== 'field'))
-            delete newPlayer.status;
-        if (imageUrl === '')
-            delete newPlayer.imageUrl;
-        if (teamId === '')
-            delete newPlayer.teamId;
+  async function handleSubmit(event) {
+    event.preventDefault();
 
-        console.log(newPlayer);
-        try {
-            const response = await fetch(`${APIURL}players/`,
-                {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify(newPlayer)
-                });
-            const result = await response.json();
-            console.log(result);
-            return result;
-        } catch (error) {
-            console.log(error);
-        }
+    const newPlayer = { name, breed, status, imageUrl, teamId };
+
+    if (status !== "bench" && status !== "field") {
+      delete newPlayer.status;
     }
-    return (
-        <form onSubmit={handleSubmit}>
-            <label>
-                Name:
-                <input type="text"
-                    placeholder="required"
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                    required
-                />
-            </label>
-            <label>
-                Breed:
-                <input type="text"
-                    placeholder="required"
-                    value={breed}
-                    onChange={(e) => setBreed(e.target.value)}
-                    required
-                />
-            </label>
-            <label>
-                Status:
-                <select value={status} onChange={(e) => setStatus(e.target.value)}>
-                    <option value="bench">bench</option>
-                    <option value="field">field</option>
-                </select>
-            </label>
-            <label>
-                ImageUrl:
-                <input type="url"
-                    placeholder="optional"
-                    value={imageUrl}
-                    onChange={(e) => setImgUrl(e.target.value)}
-                />
-            </label>
-            <label>
-                TeamId:
-                <input type="number"
-                    placeholder="optional"
-                    value={teamId}
-                    onChange={(e) => setTeamId(e.target.value)}
-                />
-            </label>
+    if (imageUrl === "") {
+      delete newPlayer.imageUrl;
+    }
+    if (teamId === "") {
+      delete newPlayer.teamId;
+    }
 
-            <button type='submit' onClick={() => navigate(-1)}>Submit</button>
-        </form>
-    )
+    console.log(newPlayer);
+    try {
+      const response = await fetch(`${APIURL}players/`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newPlayer),
+      });
+      const result = await response.json();
+      console.log(result);
+      return result;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const handleBreedChange = (event) => {
+    setBreed(event.target.value);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Name:
+        <input
+          type="text"
+          placeholder="required"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+        />
+      </label>
+      <label>
+        Breed:
+        <input
+          type="text"
+          placeholder="required"
+          value={breed}
+          onChange={handleBreedChange}
+          required
+        />
+      </label>
+      
+      <label>
+        Status:
+        <select value={status} onChange={(e) => setStatus(e.target.value)}>
+          <option value="bench">bench</option>
+          <option value="field">field</option>
+        </select>
+      </label>
+     
+      <label>
+        TeamId:
+        <input
+          type="number"
+          placeholder="optional"
+          value={teamId}
+          onChange={(e) => setTeamId(e.target.value)}
+        />
+      </label>
+
+      <button type="submit" onClick={() => navigate(-1)}>
+        Submit
+      </button>
+
+      <BreedImage breed={breed} /> {/* Use the BreedImage component */}
+      
+    </form>
+  );
 }
